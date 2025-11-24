ba824e3f9ca0ab2879931c1b6fbfb485604c328c
Tronx Group <tronxgroupspa@gmail.com>
2025-11-24 15:09:13 -0300

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://magiaimaginacion.cl";
+  const lastModified = new Date();
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/privacidad-condiciones`,
+      lastModified,
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
+  ];
+}
